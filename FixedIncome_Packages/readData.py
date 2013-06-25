680812b6d0a67e1447928ba2965637348944bfb2
wangjuestam <wangjuestam@gmail.com>
2013-06-25 10:05:22 -0700

    modified readFile module

--- FixedIncome_Packages/readData.py
+++ FixedIncome_Packages/readData.py
@@ -4,15 +4,10 @@ Created on Jun 18, 2013
 @author: Billy
 '''
 
-import csv
-
-def readFile (fileName):
-    T = []
-    DF = []
+def readFile (fileName,nl_skip):
+    
     with open(fileName,'rb') as file:
-        reader = csv.reader(file)
-        reader.next()
-        for line in reader:
-            T.append(float(line[0]))
-            DF.append(float(line[1])/100)
-    return T, DF
+        lines=[line.strip().split(',') for line in file.readlines()[nl_skip:]]
+        T,DF=zip(*lines)
+    
+    return T,DF
