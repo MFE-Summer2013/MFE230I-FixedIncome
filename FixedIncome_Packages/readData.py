c06d159e21504b4641845ed5c1faf67bcd77c9dc
Billy <oxygen.billy@gmail.com>
2013-06-26 09:11:53 -0700

    New Bond and Yield

--- FixedIncome_Packages/readData.py
+++ FixedIncome_Packages/readData.py
@@ -7,8 +7,8 @@ Created on Jun 18, 2013
 import numpy as np
 
 def readFile (fileName,nl_skip=0):
-    with open(fileName,'rb') as file:
-        lines=[line.strip().split(',') for line in file.readlines()[nl_skip:]]
+    with open(fileName,'rb') as f:
+        lines=[line.strip().split(',') for line in f.readlines()[nl_skip:]]
     
     T,DF=zip(*lines)
     return np.array(T).astype(float).tolist(), (np.array(DF).astype(float)/100).tolist()
